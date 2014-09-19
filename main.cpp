f233140c9ee2f901f69ceb51d528f14b9ea5aa0e
NBijen <b.nigon@free.fr>
2014-09-19 16:44:23 +0200

    Ajout de Mars
    Done

--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@
 
 int main(int argc, char** argv)
 {
-   // CrÃ©ation du systeme
-	Solar_system solar_system;
-   solar_system.addStatic(Planet("Sun", 1.3271244004193938e11, 6.963e5, 0, 0));
-   solar_system.addPlanet("Earth", 398600.440, 6371.01);
+   // Création du systeme
+    Solar_system solar_system;
+    solar_system.addStatic(Planet("Sun", 1.3271244004193938e11, 6.963e5, 0, 0));
+    solar_system.addPlanet("Earth", 398600.440, 6371.01);
+    solar_system.addPlanet("Mars", 42828.3, 3389.9);
 
    // Initialisation
    std::cout << "Lapin" << std::endl;
