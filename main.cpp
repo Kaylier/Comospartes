ddce97a669ee8ef6b67b9f88bff1a68ff3a32977
Kaylier <kaylier@orange.fr>
2014-09-19 14:52:39 +0100

    Création du main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+#include "SolarSystem.h"
+
+int main(int argc, char** argv)
+{
+   // Création du systeme
+	Solar_system solar_system;
+   solar_system.addStatic(Planet("Sun", 1.3271244004193938e11, 6.963e5, 0, 0));
+   solar_system.addPlanet("Earth", 398600.440, 6371.01);
+
+   // Initialisation
+   std::cout << "Lapin" << std::endl;
+	return 0;
+}
