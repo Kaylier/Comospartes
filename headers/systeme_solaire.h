50fe939d4096f67dbc579b9ffe3b33627813bb24
Kaylier <kaylier@orange.fr>
2014-09-26 16:31:12 +0100

    Ecriture des fonctions getPlanet... dans Systeme_Solaire

--- headers/systeme_solaire.h
+++ headers/systeme_solaire.h
@@ -32,12 +32,12 @@ public:
    Systeme_Solaire();
    ~Systeme_Solaire();
 
-   std::vector<Planete*> get_planets();
+   std::vector<Planete*> get_planets() const;
    Planete* add_planet(Planete* planete);
    Planete* add_planet(std::string nom, double gm, double rayon, bool fixe=false);
    Planete* add_depart(Planete* planete);
    Planete* add_depart(std::string nom, double gm, double rayon, bool fixe=false);
-   void rmPlanet(Planete* planet);
+   void rmPlanet(Planete* planete);
 
    double estimer_trajectoire(Dynamique condition_initiale);
    std::vector<Dynamique> optimiser_trajet(Dynamique condition_initiale); // cherche la meilleure trajectoire (condition_initiale est la base de la recherche)
--- sources/systeme_solaire.cpp
+++ sources/systeme_solaire.cpp
@@ -1,3 +1,62 @@
 #include "../headers/systeme_solaire.h"
 
+Systeme_Solaire::Systeme_Solaire()
+{
+   _depart = NULL;
+}
+Systeme_Solaire::~Systeme_Solaire()
+{
+   for (int i=0 ; i < _planetes.size() ; ++i)
+      if (_planetes[i])
+         delete _planetes[i];
+   if (_depart)
+      delete _depart;
+}
+
+std::vector<Planete*> Systeme_Solaire::get_planets() const
+{
+   return _planetes;
+}
+Planete* Systeme_Solaire::add_planet(Planete* planete)
+{
+   _planetes.push_back(planete);
+   return planete;
+}
+Planete* Systeme_Solaire::add_planet(std::string nom, double gm, double rayon, bool fixe=false)
+{
+   Planete* planete = new Planete;
+   planete->nom = nom;
+   planete->gm = gm;
+   planete->rayon = rayon;
+   planete->fixe = fixe;
+   _planetes.push_back(planete);
+   return planete;
+}
+Planete* Systeme_Solaire::add_depart(Planete* planete)
+{
+   _planetes.push_back(planete);
+   _depart = planete;
+   return planete;
+}
+Planete* Systeme_Solaire::add_depart(std::string nom, double gm, double rayon, bool fixe=false)
+{
+   Planete* planete = new Planete;
+   planete->nom = nom;
+   planete->gm = gm;
+   planete->rayon = rayon;
+   planete->fixe = fixe;
+   _planetes.push_back(planete);
+   _depart = planete;
+   return planete;
+}
+void Systeme_Solaire::rmPlanet(Planete* planete)
+{
+   for (int i=0 ; i < planete.size() ; ++i)
+      if (_planete[i] == planete)
+      {
+         _planete.erase(_planete.begin()+i);
+         i--;
+      }
+}
+
 
