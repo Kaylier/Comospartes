25bce16b8334794d18d567413a19ad8c177daa2b
NBijen <b.nigon@free.fr>
2014-11-14 16:48:33 +0100

    DEBUGGAGE PARTIE I
    LA PARTIE II SERA PIRE

--- headers/situation.h
+++ headers/situation.h
@@ -1,21 +1,25 @@
+#ifndef SITUATION_H
+#define SITUATION_H
 #include <map>
+#include "coord.h"
+#include "structs.h"
+
 
 class Situation
    {
    protected:
-      map<double, map<*Planete, Cinematique>> Position; //Système
+      std::map<double, std::map<Planete*, Cinematique> > Position; //Système
       double pas;
 
    public:
       Situation();
-      Situation(double tInital, std::map<Planetes, Cinematique> sitInital);
+      Situation(double tInital, std::map<Planete*, Cinematique> sitInital, double pasInitial);
       double tempsproche(double temps);
       void ajouterTemps(double temps); //Ajoute l'ensemble des coordonées à un temps voulu
       Coord<3> getForce (Coord<3>, double temps);// Forces s'appliquant en un point
-      Cinematique getCinem (*Planete planete, double temps);
+      Cinematique& getCinem (Planete* planete, double temps);
 
    };
 
-double tempsmax (*Situation systeme);
 
-
+#endif
